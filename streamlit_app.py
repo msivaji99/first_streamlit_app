8fb79292af2372dcbd100a919546b2063550b1fb
Siva M <67194585+msivaji99@users.noreply.github.com>
2022-05-29 17:27:38 +0530

    Update streamlit_app.py
    Menu Items font size changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents healthy dinner')
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeat')
-streamlit.title('Kale, Spinach & Rocket Smoothing')
-streamlit.title('Hard-boiled Free-range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeat')
+streamlit.text('Kale, Spinach & Rocket Smoothing')
+streamlit.text('Hard-boiled Free-range Egg')
